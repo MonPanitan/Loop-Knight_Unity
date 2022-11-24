2eb0acd53158f3b42f78bdc9c3e89502a9b8bdc9
MonPanitan <panitanire@gmail.com>
2022-11-24 22:47:29 +0000

    Player and Enemy can Die
    Update when player hit enemy can die also player can die from enemiy hit

--- Loop Knight/Assets/Script/EnemyWalk.cs
+++ Loop Knight/Assets/Script/EnemyWalk.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class EnemyWalk : MonoBehaviour
 {
+    //Check if enemy die
+    public bool enemyAlive = true;
+
     private GameObject player;
     private Animator enemyAction;
     private Rigidbody enemyRb;
 
     private float speed = 2.0f;
+    public int enemyHealth;
 
     //Player Position
     private Vector3 playerPos;
@@ -19,13 +23,14 @@ public class EnemyWalk : MonoBehaviour
     private float coolDown; // Cooldown time to set to be able to attack //
 
 
-    //Check the distance
+    //Check the distance between player and enemy.
     public float distanceBetween;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyHealth = 4;
         player = GameObject.Find("Player");
         enemyAction = GetComponent<Animator>();
         enemyRb = GetComponent<Rigidbody>();
@@ -36,49 +41,49 @@ public class EnemyWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Keep updating player position
-        playerPos = player.transform.position; // get a player position
-
-        distanceBetween = Vector3.Distance(transform.position, playerPos);
-
-        //Looking at player
-        Vector3 lookDirection = (playerPos - transform.position).normalized;
-        transform.LookAt(playerPos);
-
-        if (distanceBetween < 20)
+        if(enemyAlive == true)
         {
-            playerPos.y = 0; //Ignore height
-            
-            if(distanceBetween > 2)
-            {
-                moveToPlayer();
-                if(distanceBetween < 2)
-                {
-                    enemyAction.SetTrigger("Idle");
+            //Keep updating player position
+            playerPos = player.transform.position; // get a player position
 
-                }
-            }
-            if (distanceBetween <= attackDistance)
-            {
-                if (attackTimerCountDown > 0) // Check if timer more than 0
-                {
-                    attackTimerCountDown -= Time.deltaTime; // will reduce timer every 1 unit per frame
-                }
-                if (attackTimerCountDown < 0)// Check if timer == 0. Will to allow to attack
-                {
-                    attackTimerCountDown = 0;
-                }
-                if (attackTimerCountDown == 0)
-                {
-                    enemyAction.SetLayerWeight(1, 1);
-                    attack();
-                    Debug.Log("Playet Got A HIT!!!");
-                    attackTimerCountDown = coolDown;
-                }
-                
+            distanceBetween = Vector3.Distance(transform.position, playerPos);
 
+            //Looking at player
+            Vector3 lookDirection = (playerPos - transform.position).normalized;
+            transform.LookAt(playerPos);
+            if (distanceBetween < 20)
+            {
+                playerPos.y = 0; //Ignore height
+
+                if (distanceBetween > 2)
+                {
+                    moveToPlayer();
+                    if (distanceBetween < 2)
+                    {
+                        enemyAction.SetTrigger("Idle");
+
+                    }
+                }
+                if (distanceBetween <= attackDistance)
+                {
+                    if (attackTimerCountDown > 0) // Check if timer more than 0
+                    {
+                        attackTimerCountDown -= Time.deltaTime; // will reduce timer every 1 unit per frame
+                    }
+                    if (attackTimerCountDown < 0)// Check if timer == 0. Will to allow to attack
+                    {
+                        attackTimerCountDown = 0;
+                    }
+                    if (attackTimerCountDown == 0)
+                    {
+                        enemyAction.SetLayerWeight(1, 1);
+                        attack();
+                        attackTimerCountDown = coolDown;
+                    }
+                }
             }
         }
+        
     }
     private void moveToPlayer()
     {
@@ -87,15 +92,29 @@ public class EnemyWalk : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime); // move towards player position
         /*transform.rotation = player.transform.rotation;*/
         //transform.forward = playerPos - transform.position;//turn(facing) towards player // This make enemy glichy when near player
-        
-        
-
-        
     }
 
     private void attack()
     {
         enemyAction.SetTrigger("attack1");
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sword")
+        {
+            enemyHealth -= 1;
+            if (enemyHealth == 0)
+            {
+                enemyHealth = 0;
+                enemyDie();
+            }
+        }
+    }
+    private void enemyDie()
+    {
+        enemyAlive = false;
+        enemyAction.SetLayerWeight(2, 1);
+        enemyAction.SetTrigger("Die");
     }
 }
